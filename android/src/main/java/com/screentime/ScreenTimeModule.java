6b8e6dc61f4dd5320f15b58a95fecda946417937
Addin Gama Bertaqwa <addingama@gmail.com>
2023-02-23 22:41:44 +0800

    feat: implementation of usage access module on Android

--- android/src/main/java/com/screentime/ScreenTimeModule.java
+++ android/src/main/java/com/screentime/ScreenTimeModule.java
@@ -1,19 +1,43 @@
 package com.screentime;
 
+import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.facebook.react.bridge.WritableNativeMap;
 import com.facebook.react.module.annotations.ReactModule;
+
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @ReactModule(name = ScreenTimeModule.NAME)
 public class ScreenTimeModule extends ReactContextBaseJavaModule {
   public static final String NAME = "ScreenTime";
+  public Context context;
 
   public ScreenTimeModule(ReactApplicationContext reactContext) {
     super(reactContext);
+    context = reactContext;
   }
 
   @Override
@@ -22,11 +46,99 @@ public class ScreenTimeModule extends ReactContextBaseJavaModule {
     return NAME;
   }
 
+  /**
+   * check if the application info is still existing in the device / otherwise it's not possible to show app detail
+   * @return true if application info is available
+   */
+  private boolean isAppInfoAvailable(UsageStats usageStats) {
+    try {
+      getReactApplicationContext().getPackageManager().getApplicationInfo(usageStats.getPackageName(), 0);
+      return true;
+    } catch (PackageManager.NameNotFoundException e) {
+      return false;
+    }
+  }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
+  /**
+   * check if PACKAGE_USAGE_STATS permission is allowed for this application
+   * @return true if permission granted
+   */
+  private boolean getGrantStatus() {
+    AppOpsManager appOps = (AppOpsManager) getReactApplicationContext()
+      .getSystemService(Context.APP_OPS_SERVICE);
+
+    int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS,
+      android.os.Process.myUid(), getReactApplicationContext().getPackageName());
+
+    if (mode == AppOpsManager.MODE_DEFAULT) {
+      return (getReactApplicationContext().checkCallingOrSelfPermission(android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
+    } else {
+      return (mode == MODE_ALLOWED);
+    }
+  }
+
+  /**
+   * helper method to get string in format hh:mm:ss from miliseconds
+   *
+   * @param millis (application time in foreground)
+   * @return string in format hh:mm:ss from miliseconds
+   */
+  private String getDurationBreakdown(long millis) {
+    if (millis < 0) {
+      throw new IllegalArgumentException("Duration must be greater than zero!");
+    }
+
+    long hours = TimeUnit.MILLISECONDS.toHours(millis);
+    millis -= TimeUnit.HOURS.toMillis(hours);
+    long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+    millis -= TimeUnit.MINUTES.toMillis(minutes);
+    long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+    return (hours + " h " +  minutes + " m " + seconds + " s");
+  }
+
   @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+  public void getUsageStats(ReadableMap timeRange, Promise promise) {
+    double startTime = timeRange.getDouble("startTime");
+    double endTime = timeRange.getDouble("endTime");
+
+    UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+    List<UsageStats> usageStatsList = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, (long) startTime, (long) endTime);
+
+    WritableArray usageStatsArray = new WritableNativeArray();
+
+    for (UsageStats usageStats : usageStatsList) {
+      WritableMap appUsage = new WritableNativeMap();
+      if (usageStats.getTotalTimeInForeground() > 0) {
+        appUsage.putString("packageName", usageStats.getPackageName());
+        appUsage.putDouble("totalTimeInForeground", usageStats.getTotalTimeInForeground());
+        appUsage.putDouble("lastTimeUsed", usageStats.getLastTimeUsed());
+        appUsage.putString("duration", getDurationBreakdown(usageStats.getTotalTimeInForeground()));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          appUsage.putDouble("totalTimeVisible", usageStats.getTotalTimeVisible());
+        }
+        usageStatsArray.pushMap(appUsage);
+      }
+
+    }
+
+    promise.resolve(usageStatsArray);
+  }
+
+
+
+  @ReactMethod
+  public void openUsageSettings(Promise promise) {
+    Intent i = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    i.addFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+    context.startActivity(i);
+    promise.resolve(true);
+  }
+
+  @ReactMethod
+  public void getGrantStatus(Promise promise) {
+    promise.resolve(getGrantStatus());
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,84 @@ const ScreenTime = NativeModules.ScreenTime
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return ScreenTime.multiply(a, b);
+/**
+ * UsageData type that represent app usages
+ */
+export type UsageData = {
+  packageName: string;
+  totalTimeInForeground: number;
+  totalTimeVisible?: number;
+  lastTimeUsed?: number;
+  duration?: string;
+};
+
+type GetUsageStatsParams = {
+  startTime: number;
+  endTime: number;
+};
+
+/**
+ * [Android] Function to open Usage Settings screen on the device so that user can enable the permission for the app.
+ *
+ * @returns void
+ */
+export function openUsageSettings(): Promise<boolean> {
+  if (Platform.OS === 'android') {
+    return ScreenTime.openUsageSettings();
+  }
+
+  return new Promise((resolve) => resolve(false));
+}
+
+/**
+ * [Android] Function to get Usage Settings grant status.
+ *
+ * @returns void
+ */
+export function getGrantStatus(): Promise<boolean> {
+  if (Platform.OS === 'android') {
+    return ScreenTime.getGrantStatus();
+  }
+
+  return new Promise((resolve) => resolve(false));
+}
+
+/**
+ * [Android] Function to get Usage Settings grant status.
+ *
+ * The data is sorted by totalTimeInForeground descending
+ * @param {GetUsageStatsParams} params
+ * @param {number} params.startTime - Start time in miliseconds
+ * @param {number} params.endTime - End time in miliseconds
+ * @returns {Promise<UsageData[]>} - Array of usage data
+ */
+export async function getUsageStats(
+  params: GetUsageStatsParams
+): Promise<UsageData[]> {
+  if (Platform.OS === 'android') {
+    const result: UsageData[] = await ScreenTime.getUsageStats(params);
+
+    // There are possibility for the duplicate package name, so it's better to combine the usage time
+    const packageNames = [...new Set(result.map((item) => item.packageName))];
+
+    const combinedTimes: UsageData[] = [];
+    packageNames.forEach((packageName) => {
+      const filtered = result.filter(
+        (item) => item.packageName === packageName
+      );
+      let sum = 0;
+      filtered.forEach((item) => (sum += item.totalTimeInForeground));
+      combinedTimes.push({
+        packageName,
+        totalTimeInForeground: sum,
+      });
+    });
+    // sort the data so that most used app will be the first
+    combinedTimes.sort(
+      (a, b) => b.totalTimeInForeground - a.totalTimeInForeground
+    );
+    return new Promise((resolve) => resolve(combinedTimes));
+  }
+
+  return new Promise((resolve) => resolve([]));
 }
